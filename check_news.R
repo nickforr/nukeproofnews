8f5b34e667f9ba6d7dcc6a3662ef976a5d3354a0
Nick Forrester <nickforr@me.com>
2020-09-23 22:48:29 +0100

    write to json

--- check_news.R
+++ check_news.R
@@ -2,10 +2,13 @@
 if (!requireNamespace("xml2")) {
   install.packages("xml2")
 }
+if (!requireNamespace("jsonlite")) {
+  install.packages("jsonlite")
+}
 
 nkp_url <- "http://nukeproof.com/wp-sitemap-posts-post-1.xml"
 nkp_xml <- xml2::read_xml(nkp_url)
 nkp <- xml2::xml_contents(nkp_xml)
 nkp_news <- xml2::xml_text(nkp)
 tail(nkp_news)
-
+jsonlite::write_json(tail(nkp_news), "news.json")
