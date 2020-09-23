a23a1fc4bcaa767d0003eec944c422d684506869
Nick Forrester <nickforr@me.com>
2020-09-24 00:15:56 +0100

    use ifttt

--- check_news.R
+++ check_news.R
@@ -1,14 +1,36 @@
+#!/usr/bin/env Rscript
+maker_key = commandArgs(trailingOnly = TRUE)
+if (length(maker_key) != 1) {
+  stop("key must be provided", call. = FALSE)
+}
 
 if (!requireNamespace("xml2")) {
   install.packages("xml2")
 }
-if (!requireNamespace("jsonlite")) {
-  install.packages("jsonlite")
+if (!requireNamespace("httr")) {
+  install.packages("httr")
 }
+
+my_event <- 'nukeproof_news'
 
 nkp_url <- "http://nukeproof.com/wp-sitemap-posts-post-1.xml"
 nkp_xml <- xml2::read_xml(nkp_url)
 nkp <- xml2::xml_contents(nkp_xml)
 nkp_news <- xml2::xml_text(nkp)
-tail(nkp_news)
-jsonlite::write_json(tail(nkp_news), "news.json")
+latest_news <- rev(basename(tail(nkp_news, 3)))
+
+maker_url <- 
+  paste(
+    'https://maker.ifttt.com/trigger', 
+    my_event, 
+    'with/key',
+    maker_key, 
+    sep = '/'
+  )
+maker_body <- 
+  list(
+    value1 = latest_news[1],
+    value2 = latest_news[2], 
+    value3 = latest_news[3]
+  )
+httr::POST(maker_url, body = maker_body)
